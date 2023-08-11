1047dea8c250c58f1994a488abf5de85a6272011
Stefanie Sarközi <sarkoezi.stefanie@gmail.com>
2023-08-11 22:20:54 +0200

    add form upd

--- src/Form/EventsType.php
+++ src/Form/EventsType.php
@@ -12,9 +12,29 @@ class EventsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('dateNTime')
-            ->add('description')
+            ->add('name', TextareaType::class, [
+                'attr' => ['class' => 'form-control formName']
+            ]);
+
+        $builder
+            ->add('dateNTime', DateTimeType::class, [ 
+                'attr' => ['class' => 'form-control formDate'], 
+                'widget' => 'single_text',
+                'days' => range(1,31),
+                'placeholder' => [
+                    'year' => 'Year', 'month' => 'Month', 'day' => 'Day',
+                    'hour' => 'Hour', 'minute' => 'Minute', 'second' => 'Second',
+                ],
+                'date_label' => 'Date/Time:'
+            ]);
+
+        $builder
+            ->add('description', TextareaType::class, [
+                'attr' => ['class' => 'form-control formDescription'],
+                'label' => 'Description'
+            ]);
+
+         $builder
             ->add('image')
             ->add('capacity')
             ->add('email')
