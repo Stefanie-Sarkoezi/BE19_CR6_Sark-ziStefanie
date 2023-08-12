89a7b6c6c9666f05d2fab50c69e58c8e93af4cbc
Stefanie Sarközi <sarkoezi.stefanie@gmail.com>
2023-08-12 17:28:53 +0200

    add design

--- src/Form/EventsType.php
+++ src/Form/EventsType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Events;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,70 +13,52 @@ class EventsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, [
-                'attr' => ['class' => 'form-control formName'],
+            ->add('name', null, [
+                'attr' => ['class' => 'form-control formName my-3'],
                 'label' => 'Event Name:'
-            ]);
-
-        $builder
-            ->add('dateNTime', DateTimeType::class, [ 
-                'attr' => ['class' => 'form-control formDate'], 
+            ])
+            ->add('dateNTime', null, [ 
+                'attr' => ['class' => 'form-control formDate my-3'], 
                 'widget' => 'single_text',
                 'days' => range(1,31),
                 'placeholder' => [
                     'year' => 'Year', 'month' => 'Month', 'day' => 'Day',
                     'hour' => 'Hour', 'minute' => 'Minute', 'second' => 'Second',
                 ],
-                'date_label' => 'Date/Time:'
-            ]);
-
-        $builder
-            ->add('description', TextareaType::class, [
-                'attr' => ['class' => 'form-control formDescription'],
+                'label' => 'Date/Time:'
+            ])
+            ->add('description', null, [
+                'attr' => ['class' => 'form-control formDescription my-3'],
                 'label' => 'Description:'
-            ]);
-
-         $builder
-            ->add('image', TextType::class, [
-                'attr' => ['class' => 'formImage form-control'],
+            ])
+            ->add('image', null, [
+                'attr' => ['class' => 'formImage form-control my-3'],
                 'label' => 'Image:'    
-            ]);
-
-         $builder
-            ->add('capacity', IntegerType::class, [
-                'attr' => ['class' => 'formCapacity form-control'],
+            ])
+            ->add('capacity', null, [
+                'attr' => ['class' => 'formCapacity form-control my-3'],
                 'label' => 'Capacity:' 
-            ]);
-
-        $builder   
-            ->add('email', EmailType::class, [
-                'attr' => ['class' => 'formMail form-control'],
+            ])   
+            ->add('email', null, [
+                'attr' => ['class' => 'formMail form-control my-3'],
                 'label' => 'Email:',
-                'placeholder' => 'example@gmail.com'
-            ]);
-
-        $builder
-            ->add('phoneNumber', TelType::class, [
-                'attr' => ['class' => 'formPhone form-control'],
+    
+            ])
+            ->add('phoneNumber', null, [
+                'attr' => ['class' => 'formPhone form-control my-3'],
                 'label' => 'Phone Number:',
-            ]);
-
-        $builder
-            ->add('address', TextType::class, [
-                'attr' => ['class' => 'formAddress form-control'],
+            ])
+            ->add('address', null, [
+                'attr' => ['class' => 'formAddress form-control my-3'],
                 'label' => 'Address:'
-            ]);
-
-        $builder
-            ->add('url', UrlType::class, [
-                'attr' => ['class' => 'formUrl form-control'],
+            ])
+            ->add('url', null, [
+                'attr' => ['class' => 'formUrl form-control my-3'],
                 'label' => 'Event Url:',
-                'placeholder' => 'www.event-page.com'
-            ]);
-
-        $builder
+               
+            ])
             ->add('type', ChoiceType::class , [ 
-                'attr' => ['class' => 'form-control mt-2 formType'],
+                'attr' => ['class' => 'form-control my-3 formType'],
                 'label' => 'Event Type:',
                 'choices'  => [ 
                     'Music' => 'Music',
